f7211883873f607d3cd6a8a397db67392b8fe4db
techforce <italobonilha7@gmail.com>
2024-10-07 16:25:52 -0300

    hello world!

---
+++ src/main/java/projeto/com/br/italobonilha/ItalobonilhaApplication.java
@@ -0,0 +1,13 @@
+package projeto.com.br.italobonilha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItalobonilhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItalobonilhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/com/br/italobonilha/ItalobonilhaApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.com.br.italobonilha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalobonilhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
